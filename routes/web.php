791d4eae0ad3a1b90a584f6b72621fa14c01cddd
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-04-13 10:13:34 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GuestController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,12 +16,28 @@ use App\Http\Controllers\GuestController;
 Route::get('/', function () {
     return view('welcome');
 });
-Livewire::setScriptRoute(function ($handle) {
-    return Route::get('/marinoV2/public/livewire/livewire.js', $handle);
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Livewire::setUpdateRoute(function ($handle) {
-    return Route::post('/marinoV2/public/livewire/update', $handle);
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
+    Livewire::setScriptRoute(function ($handle) {
+        return Route::get('/marinoV2/public/livewire/livewire.js', $handle);
+    });
+    
+    Livewire::setUpdateRoute(function ($handle) {
+        return Route::post('/marinoV2/public/livewire/update', $handle);
+    });
+    
 require __DIR__.'/auth.php';
+
+Route::get('/', [GuestController::class, 'index'])->name('home');
+Route::get('/about-us', [GuestController::class, 'aboutUs'])->name('about-us');
+Route::get('/products', [GuestController::class, 'products'])->name('products');
+Route::get('/clients', [GuestController::class, 'clients'])->name('clients');
+Route::get('/contact', [GuestController::class, 'contact'])->name('contact');
+Route::post('/check', [GuestController::class, 'checkEmail'])->name('check-email');
+Route::post('/request', [GuestController::class, 'requestLoad'])->name('request');
