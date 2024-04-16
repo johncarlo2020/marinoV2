6dac81cd04e671094ca2299055ebba526e5ab2c9
whei2419 <joelnacionales24@gmail.com>
2024-04-17 04:26:42 +0800

    add sample layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,56 @@
 <x-app-layout>
     <div class="container pt-4">
-        test
+        <div class="p-4 border rounded d-flex justify-content-between">
+            <div>
+                <div class="current-balance">
+                    <h1>342342342342</h1>
+                    <p>Current ballance in wallet</p>
+                </div>
+            </div>
+            <div class="gap-3 d-flex">
+                <button class="btn btn-warning btn-lg">Request funds</button>
+                <button class="btn btn-primary btn-lg">Request load</button>
+            </div>
+        </div>
+        <div class="p-4 mt-3 border rounded">
+            <div class="col">
+                best deals
+            </div>
+        </div>
+
+        <div class="p-4 mt-3 border rounded">
+            <h2>Transaction details</h2>
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
+
     </div>
 
 </x-app-layout>
