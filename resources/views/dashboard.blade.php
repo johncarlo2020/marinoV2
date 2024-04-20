ded9fb2b0ac6da1ebbee5f395717e720df6edf95
whei2419 <joelnacionales24@gmail.com>
2024-04-21 06:52:34 +0800

    add new styles and set up modal files

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,64 @@
 <x-app-layout>
-    <div class="container pt-4">
-        <div class="gap-4 d-flex justify-content-between">
-            <button class="tile-btn">
-                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt=""> <span class="title">Request funds</span>
-            </button>
-            <button class="tile-btn">
-                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt=""> <span class="title">Request funds</span>
-            </button>
-            <button class="tile-btn">
-                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt=""> <span class="title">Request funds</span>
-            </button>
+    <div class="p-0 container-fluid">
+        <div class="balance-section">
+            <div class="switch-container">
+
+            </div>
+            <div class="balance-content">
+                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt="">
+                <span class="balance-text">
+                    <span class="balance">$ 121232 23</span>
+                    <span class="sub-text"><small>Current balance on wallet</small></span>
+                </span>
+            </div>
         </div>
-        <div class="mt-3 transaction-table">
+    </div>
+    <div class="container mt-2">
+        <div class="gap-2 p-2 row">
+            <div class="p-0 col">
+                <button class="tile-btn" onclick="openModal('topUpModal')">
+                    <i class="fa-solid fa-circle-plus"></i> <span class="title">Top up</span>
+                </button>
+                </button>
+            </div>
+            <div class="p-0 col">
+                <button class="tile-btn">
+                    <i class="fa-solid fa-sim-card"></i> <span class="title">Load self</span>
+                </button>
+            </div>
+            <div class="p-0 col">
+                <button class="tile-btn">
+                    <i class="fa-solid fa-file-circle-plus"></i></i> <span class="title">New load</span>
+                </button>
+            </div>
+        </div>
+        <div class="mt-2 transaction-table">
             <div class="header-table">
                 <div class="header-title">
                     <h2><i class="fa-solid fa-clock-rotate-left"></i> Transaction history</h2>
+                    <a class="see-all-btn" href="">See all</a>
+                </div>
+            </div>
+            <div class="table-row-container">
+                <div class="details">
+                    <div class="type">
+                        <i class="fa-solid fa-plus"></i>
+                    </div>
+                    <div class="text">
+                        <span>
+                            Top up
+                        </span>
+                        <span class="date d-block">
+                            December 12, 2021
+                        </span>
+                    </div>
+                </div>
+                <div class="amount-row">
+                    <span>-$23232</span>
                 </div>
             </div>
 
+
         </div>
     </div>
-
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <div class="dashboard-nav">
-    <div class="container p-3 d-flex justify-content-between align-items-center">
+    <div class="container p-2 p-md-3 d-flex justify-content-between align-items-center">
         <div class="logo">
             <a class="gap-2 navbar-brand d-flex align-items-center" href="{{ route('dashboard') }}">
                 <img src="{{ asset('images/logo.png') }}" alt="Logo" width="33">
@@ -7,15 +7,11 @@
             </a>
         </div>
         <div class="gap-3 nav-contanainer d-flex align-items-center">
-            <div class="gap-1 balance-container d-flex align-items-center">
-                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt="">
-                <span><span class=""><i class="fa-solid fa-peso-sign money-icon"></i></span> 231,234,42</span>
-            </div>
             <div class="notification">
                 <span class="notif-icon"><i class="fa-solid fa-bell"></i></span>
             </div>
             <div id="dashboardDropdown" class="dropdown">
-                <button class="" type="button" id="links" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="d-none d-md-block" type="button" id="links" data-bs-toggle="dropdown" aria-expanded="false">
                     <div class="mr-1 avatar d-inline-block">
                         <span>
                             {{ substr(Auth::user()->name, 0, 1) }}
@@ -26,7 +22,10 @@
                     </span>
                     <span><i class="fa-solid fa-caret-down"></i></span>
                 </button>
-                <ul class="shadow-sm dropdown-menu dashboard-menus" aria-labelledby="links">
+                <button class="mobile-menu d-block d-sm-block d-md-none" type="button" id="links" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+                <ul class="dropdown-menu dashboard-menus" aria-labelledby="links">
                   <li><a class="dropdown-item" href="{{route('profile.edit')}}"> {{ __('Profile') }}</a></li>
                   <li><a class="dropdown-item" href="{{route('logout')}}"  onclick="event.preventDefault();
                     this.closest('form').submit();"> {{ __('Log Out') }}</a></li>
