8441720d1bc10aca3a0df48f38b4235ab481950d
whei2419 <joelnacionales24@gmail.com>
2024-04-20 10:34:18 +0800

    update style on dashboard and nav

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,56 +1,24 @@
 <x-app-layout>
     <div class="container pt-4">
-        <div class="p-4 border rounded d-flex justify-content-between">
-            <div>
-                <div class="current-balance">
-                    <h1>342342342342</h1>
-                    <p>Current ballance in wallet</p>
+        <div class="gap-4 d-flex justify-content-between">
+            <button class="tile-btn">
+                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt=""> <span class="title">Request funds</span>
+            </button>
+            <button class="tile-btn">
+                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt=""> <span class="title">Request funds</span>
+            </button>
+            <button class="tile-btn">
+                <img src="{{ asset('images/icons/solar_wallet-bold.svg')}}" alt=""> <span class="title">Request funds</span>
+            </button>
+        </div>
+        <div class="mt-3 transaction-table">
+            <div class="header-table">
+                <div class="header-title">
+                    <h2><i class="fa-solid fa-clock-rotate-left"></i> Transaction history</h2>
                 </div>
             </div>
-            <div class="gap-3 d-flex">
-                <button class="btn btn-warning btn-lg">Request funds</button>
-                <button class="btn btn-primary btn-lg">Request load</button>
-            </div>
-        </div>
-        <div class="p-4 mt-3 border rounded">
-            <div class="col">
-                best deals
-            </div>
-        </div>
 
-        <div class="p-4 mt-3 border rounded">
-            <h2>Transaction details</h2>
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                  </tr>
-                </tbody>
-              </table>
         </div>
-
     </div>
 
 </x-app-layout>
