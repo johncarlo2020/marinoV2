9ea5118e357f6c119678b30f0fc3555f7d512bcb
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-04-30 02:05:06 +0800

    fix

--- app/Filament/Resources/LoadResource/Pages/ListLoads.php
+++ app/Filament/Resources/LoadResource/Pages/ListLoads.php
@@ -10,8 +10,5 @@ class ListLoads extends ListRecords
 {
     protected static string $resource = LoadResource::class;
 
-    protected function getHeaderActions(): array
-    {
-       
-    }
+   
 }
