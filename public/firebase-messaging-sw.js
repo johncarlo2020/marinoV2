6784f42f38973f95843c56b2938cfa1777d86865
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-05-02 14:34:06 +0800

    Revert "remove"
    This reverts commit 19cbea94d260bf39a91b3300b569d2db2dc6d04d.

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('Received background message ', payload);
+  
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+  
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
