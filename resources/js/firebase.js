8a3ba9bc270f2abe000f9cfd555f544aa4ad239b
whei2419 <joelnacionales24@gmail.com>
2024-05-02 10:52:01 +0800

    update

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -7,35 +7,32 @@ import { getMessaging, getToken } from "firebase/messaging";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const vapidKey = import.meta.env.VITE_APP_VAPID_KEY;
 const firebaseConfig = {
-  apiKey: "AIzaSyDD4GF3fxkwjjcH-ZJsV2TNUJybnDHzxQo",
-  authDomain: "marino-ebfa9.firebaseapp.com",
-  projectId: "marino-ebfa9",
-  storageBucket: "marino-ebfa9.appspot.com",
-  messagingSenderId: "286557377805",
-  appId: "1:286557377805:web:b879fe69564aeb0a9c8c44",
-  measurementId: "G-D7HHEJLY3G"
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_APP_ID,
+    measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const messaging = getMessaging(app);
 
 
-getToken(messaging, { vapidKey: 'BGiSfEUukXteJ2GhZ7bIu18fx8KBtJI-Y9NFXHaOLEu3PEb7H_rjRKO7nUl8b1eOw8GWlQmEoEVrcn-harGfoYQ' }).then((currentToken) => {
+getToken(messaging, { vapidKey: vapidKey }).then((currentToken) => {
   if (currentToken) {
     console.log(currentToken);
-    //store token in database
+    //store token in local storage check first if token is already stored
+    if(localStorage.getItem('fcm_token') === null){
+      localStorage.setItem('fcm_token', currentToken);
+    }
   } else {
     console.log('No registration token available. Request permission to generate one.');
   }
 }).catch((err) => {
   console.log('An error occurred while retrieving token. ', err);
 });
-
-
-
-
-
-
