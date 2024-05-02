1803f283be7a67fc292b8e01945dde059e025351
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-05-02 13:53:37 +0800

    changes

--- app/Helpers/global.php
+++ app/Helpers/global.php
@@ -4,6 +4,63 @@ use App\User;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 
+    function sendToToken($token , $data) {
+    $url = "https://fcm.googleapis.com/fcm/send";
+        $fields = array (
+            'to'            => $token,
+            'notification' => array(
+                'title' => $data['title'],
+                'body' => $data['body'],
+                'icon' => "new",
+                'sound' => "default",// Include image URL here
+            ),
+            'data' => $data['data'],
+            'apns' => 
+            array (
+              'payload' => 
+              array (
+                'aps' => 
+                array (
+                  'contentAvailable' => true,
+                ),
+              ),
+              'headers' => 
+              array (
+                'apns-push-type' => 'background',
+                'apns-priority' => '5',
+                'apns-topic' => 'io.flutter.plugins.firebase.messaging',
+              ),
+            ),
+        );
+
+        $header = array(
+            "Authorization:key=AAAAQrgmlQ0:APA91bGRbqPSFje-DxFod5fRx486KyaThOcgOTC53hEVdIE_BfNprqQq1lQmv2KLzM4HyphJO2oWWatcT-RJWfnQOZwNt5oY1uX2j3Pqu0hfIgnScetzyjQ58og91xshWTbXfwc0V4tX",
+            'Content-Type:application/json'
+        );
+
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        
+        $result = curl_exec($ch);
+
+		if ($result === false)
+        {
+            return 0;
+        }
+
+        curl_close($ch);
+
+        return 1;
+    }
+
 function load($number,$baht,$network)
 {
     $client = new Client();
