0028bb090436f54535d085017a47b2f8e4c43a28
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-05-04 12:24:44 +0800

    remove request load

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
 
         @include('section.products', ['heading' => $pruductsHeading,'description' => $productSectionDescription])
 
-        @include('section.requestLoad',['heading' => 'Request Load'],['description' => 'To complete your request, please ensure that all fields are filled out. Your cooperation in providing all necessary information is greatly appreciated and will help expedite the process. Thank you.'])
+        <!-- @include('section.requestLoad',['heading' => 'Request Load'],['description' => 'To complete your request, please ensure that all fields are filled out. Your cooperation in providing all necessary information is greatly appreciated and will help expedite the process. Thank you.']) -->
 
         @include('section.testimonial',['heading' => $testimonialHeading])
 
